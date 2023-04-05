eea85cd5f5abbb33ff03d1de0f07884290c2225b
Dan Pham <phamphuc0603@gmail.com>
2023-04-05 14:06:19 +0700

    Added Randomer

---
+++ Assets/Maniac/RandomSystem/InitRandomerService.cs
@@ -0,0 +1,17 @@
+﻿using Cysharp.Threading.Tasks;
+using Maniac.Services;
+using Maniac.Utils;
+
+namespace Maniac.RandomSystem
+{
+    public class InitRandomerService : Service
+    {
+        public override async UniTask<IService.Result> Execute()
+        {
+            var randomer = new Randomer();
+            Locator<Randomer>.Set(randomer);
+            randomer.Init();
+            return IService.Result.Success;
+        }
+    }
+}
---
+++ Assets/Maniac/RandomSystem/Randomer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Maniac.RandomSystem
+{
+    public class Randomer
+    {
+        private int _seed;
+        public int Seed => _seed;
+        
+        public void Init()
+        {
+            _seed = Random.Range(int.MinValue, int.MaxValue);
+        }
+
+        public void SetSeed(int seed = 0)
+        {
+            if(seed != 0)
+                _seed = seed;
+            
+            Random.InitState(_seed);
+        }
+    }
+}
