df75355653b3567783f667839615aecfa36e56be
Dan Pham <phamphuc0603@gmail.com>
2023-04-26 00:01:13 +0700

    Add animation to player movement

--- Assets/Game/Players/Scripts/NetPlayerController.cs
+++ Assets/Game/Players/Scripts/NetPlayerController.cs
@@ -1,4 +1,5 @@
-﻿using Maniac.DataBaseSystem;
+﻿using System;
+using Maniac.DataBaseSystem;
 using Maniac.Utils;
 using Unity.Netcode;
 using UnityEngine;
@@ -7,18 +8,19 @@ namespace Game.Players.Scripts
 {
     public class NetPlayerController : NetworkBehaviour
     {
+        private static readonly int IsMoving = Animator.StringToHash("IsMoving");
         private DataBase _dataBase => Locator<DataBase>.Instance;
         private PlayerConfig _playerConfig;
         
-        [SerializeField]
-        private Rigidbody2D rb;
-        private Transform _transform;
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private Transform graphicsHolder;
+        [SerializeField] private NetPlayerInput _netPlayerInput;
+        [SerializeField] private Animator _animator;
         private bool _movable = false;
 
         private void Awake()
         {
             _playerConfig = _dataBase.GetConfig<PlayerConfig>();
-            _transform = transform;
         }
 
         public override void OnNetworkSpawn()
@@ -30,6 +32,23 @@ namespace Game.Players.Scripts
             base.OnNetworkSpawn();
         }
 
+        private void Update()
+        {
+            if (!IsOwner) return;
+
+            UpdateCharacterGraphics();
+        }
+
+        private void UpdateCharacterGraphics()
+        {
+            // Update character graphics direction
+            var localScale = graphicsHolder.localScale;
+            localScale = new Vector3(_netPlayerInput.RawInputVector.x > 0 ? 1 : _netPlayerInput.RawInputVector.x < 0 ? -1 : graphicsHolder.localScale.x, localScale.y, localScale.z);
+            graphicsHolder.localScale = localScale;
+
+            _animator.SetBool(IsMoving, _netPlayerInput.RawInputVector != Vector2.zero);
+        }
+
         private void FixedUpdate()
         {
             if (IsOwner && _movable)
@@ -38,10 +57,7 @@ namespace Game.Players.Scripts
 
         private void UpdateMovement()
         {
-            var horizontal = Input.GetAxisRaw("Horizontal");
-            var vertical = Input.GetAxisRaw("Vertical");
-
-            rb.velocity = new Vector2(horizontal, vertical).normalized * _playerConfig.MoveSpeed;
+            rb.velocity = _netPlayerInput.RawInputVector * _playerConfig.MoveSpeed;
         }
     }
 }
---
+++ Assets/Game/Players/Scripts/NetPlayerInput.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace Game.Players.Scripts
+{
+    public class NetPlayerInput : NetworkBehaviour
+    {
+        public Vector2 RawInputVector { get; private set; }
+        public Vector2 InputVector { get; private set; }
+        
+        public override void OnNetworkSpawn()
+        {
+            if (!IsOwner)
+                enabled = false;
+
+            base.OnNetworkSpawn();
+        }
+        
+        public void Update()
+        {
+            if (IsOwner)
+            {
+                RawInputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+                InputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+            }
+        }
+    }
+}
