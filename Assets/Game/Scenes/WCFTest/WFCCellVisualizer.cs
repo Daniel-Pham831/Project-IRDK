c211a7a84b265af7b5ffa6cbcc7996e43bd0cfb3
Dan Pham <phamphuc0603@gmail.com>
2023-05-17 21:18:18 +0700

    working on WFC visualizer

---
+++ Assets/Game/Scenes/WCFTest/WFCCellVisualizer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Maniac.Utils;
+using UniRx;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Game.Scenes.WCFTest
+{
+    public class WFCCellVisualizer : MonoBehaviour
+    {
+        private WFCVisualizer _wfcVisualizer => Locator<WFCVisualizer>.Instance;
+        private WFCCell _cell;
+        [SerializeField] private SpriteRenderer mainSpriteRenderer;
+        [SerializeField] private List<SpriteRenderer> subSpriteRenderers;
+
+        public void Setup(WFCCell cell)
+        {
+            _cell = cell;
+
+            cell.FinalSprite.Subscribe(value =>
+            {
+                if (value != null)
+                {
+                    mainSpriteRenderer.sprite = value;
+                    foreach (var subSpriteRenderer in subSpriteRenderers)
+                    {
+                        subSpriteRenderer.sprite = null;
+                    }
+                }
+            }).AddTo(this);
+            
+            cell.PossibleIds.ObserveCountChanged().Subscribe(value =>
+            {
+                for (int i = 0; i < subSpriteRenderers.Count; i++)
+                {
+                    bool shouldEnable = i < value;
+                    subSpriteRenderers[i].gameObject.SetActive(shouldEnable);
+                    
+                    if(shouldEnable)
+                        subSpriteRenderers[i].sprite = _wfcVisualizer.GetSpriteWithId(cell.PossibleIds[i]);
+                }
+            }).AddTo(this);
+        }
+    }
+}
---
+++ Assets/Game/Scenes/WCFTest/WFCVisualizer.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Maniac.Utils;
+using Sirenix.OdinInspector;
+using UniRx;
+using UnityEngine;
+
+namespace Game.Scenes.WCFTest
+{
+    public class WFCSystem
+    {
+        public ReactiveProperty<WFCCell[,]> WfcCells = new ReactiveProperty<WFCCell[,]>();
+        public List<WFCCell> WfcCellsList = new List<WFCCell>();
+
+        public void Setup(int columns, int rows)
+        {
+            WfcCells.Value = new WFCCell[columns, rows];
+            for (int column = 0; column < columns; column++)
+            {
+                for (int row = 0; row < rows; row++)
+                {
+                    WfcCells.Value[column, row] = new WFCCell(column,row);
+                    WfcCellsList.Add(WfcCells.Value[column, row]);
+                }
+            }
+        }
+
+        public void Collapse(string id, int column, int row)
+        {
+            WfcCells.Value[column, row].Collapse(id);
+            
+            var topNeighbor = GetCell(column, row + 1);
+            var bottomNeighbor = GetCell(column, row - 1);
+            var leftNeighbor = GetCell(column - 1, row);
+            var rightNeighbor = GetCell(column + 1, row);
+
+            topNeighbor?.PossibleIds.Remove(id);
+            bottomNeighbor?.PossibleIds.Remove(id);
+            leftNeighbor?.PossibleIds.Remove(id);
+            rightNeighbor?.PossibleIds.Remove(id);
+        }
+
+        private WFCCell GetCell(int column, int row)
+        {
+            if (column < 0 || column >= WfcCells.Value.GetLength(0) || row < 0 || row >= WfcCells.Value.GetLength(1))
+            {
+                return null;
+            }
+            return WfcCells.Value[column, row];
+        }
+    }
+
+    public class WFCCell
+    {
+        private WFCVisualizer _wfcVisualizer => Locator<WFCVisualizer>.Instance;
+        public int Column;
+        public int Row;
+        public ReactiveCollection<string> PossibleIds = new ReactiveCollection<string>();
+        public ReactiveProperty<Sprite> FinalSprite = new ReactiveProperty<Sprite>(null);
+        
+        private string _id;
+
+        public WFCCell(int column, int row)
+        {
+            Column = column;
+            Row = row;
+            foreach (var constraint in _wfcVisualizer.Constraints)
+            {
+                PossibleIds.Add(constraint.Id);
+            }
+        }
+        
+        public void Collapse(string id)
+        {
+            _id = id;
+            FinalSprite.Value = Locator<WFCVisualizer>.Instance.GetSpriteWithId(_id);
+            PossibleIds.Clear();
+        }
+    }
+
+    public class WFCVisualizer : MonoBehaviour
+    {
+        public List<CellInfo> Constraints;
+        public int Columns = 1;
+        public int Rows = 1;
+        public float CellSize = 1f;
+        private WFCSystem _newWFCSystem;
+        public WFCCellVisualizer WfcCellVisualizerPrefab;
+        public Transform WfcCellVisualizerParent;
+
+        private void Awake()
+        {
+            Locator<WFCVisualizer>.Set(this);
+            _newWFCSystem = new WFCSystem();
+            Locator<WFCSystem>.Set(_newWFCSystem,true);
+        }
+
+        private void OnValidate()
+        {
+            Rows = Mathf.Clamp(Rows, 0, Rows);
+            Columns = Mathf.Clamp(Columns, 0, Columns);
+            CellSize = Mathf.Clamp(CellSize, 0f, CellSize);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.white;
+            for (int column = 0; column < Columns; column++)
+            {
+                for (int row = 0; row < Rows; row++)
+                {
+                    Gizmos.DrawWireCube(new Vector3(column * CellSize , row * CellSize),
+                        new Vector3(CellSize, CellSize, 0));
+                }
+            }
+        }
+
+        public Sprite GetSpriteWithId(string id)
+        {
+            return Constraints.FirstOrDefault(x => x.Id == id)?.SpriteImg;
+        }
+        
+        [Button]
+        public async void ResetWFCGrid()
+        {
+            _newWFCSystem.Setup(Columns, Rows);
+            SetupWFCVisualizer();
+        }
+
+        private void SetupWFCVisualizer()
+        {
+            foreach (var cell in _newWFCSystem.WfcCellsList)
+            {
+                var wfcCellVisualizer = Instantiate(WfcCellVisualizerPrefab, WfcCellVisualizerParent);
+                wfcCellVisualizer.Setup(cell);
+            }
+        }
+
+
+        [Serializable]
+        public class CellInfo
+        {
+            public string Id;
+            public Sprite SpriteImg;
+            public List<string> Neighbours;
+        }
+    }
+}
