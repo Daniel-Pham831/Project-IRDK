112fd6f7038e9c1e2ce8f16bf73cbec5795f1f9b
Dan Pham <phamphuc0603@gmail.com>
2023-05-31 22:23:32 +0700

    Update spawner

--- Assets/Maniac/SpawnerSystem/Spawner.cs
+++ Assets/Maniac/SpawnerSystem/Spawner.cs
@@ -1,53 +1,65 @@
+using System;
+using Maniac.Utils;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Pool;
+using Object = UnityEngine.Object;
 
 namespace Maniac.SpawnerSystem
 {
-    public class Spawner<T> where T :Object
+    public class Spawner<T> where T : Object
     {
         private readonly Object _prefab;
         private readonly ObjectPool<T> _pool;
         public ObjectPool<T> Pool => _pool;
         private int counter = 0;
-        
-        public Spawner(T prefab) 
+
+        public Spawner(T prefab)
         {
             _prefab = prefab;
             _pool = new ObjectPool<T>(CreateFunction, GetFunction, ReleaseFunction, DestroyFunction, false);
         }
-
+        
         private T CreateFunction()
         {
-            var mono = UnityEngine.Object.Instantiate(_prefab);
-            mono.name = _prefab.name + SpawnerManager.Indicator + counter++;
-            
-            return mono as T;
+            var obj = UnityEngine.Object.Instantiate(_prefab);
+            obj.name = $"{_prefab.name}{SpawnerManager.Indicator}{counter++}";
+
+            return obj as T;
         }
 
         private void GetFunction(T obj)
         {
+            if (obj == null) return;
+
             switch (obj)
             {
                 case GameObject gobj:
-                    gobj.SetActive(true);
+                    if(!gobj.activeSelf)
+                        gobj.SetActive(true);
                     break;
-                
+
                 case MonoBehaviour mono:
-                    mono.gameObject.SetActive(true);
+                    if(!mono.gameObject.activeSelf)
+                        mono.gameObject.SetActive(true);
                     break;
             }
         }
 
         private void ReleaseFunction(T obj)
         {
+            if (obj == null) return;
+
             switch (obj)
             {
                 case GameObject gobj:
-                    gobj.SetActive(false);
+                    if(gobj.activeSelf)
+                        gobj.SetActive(false);
                     break;
-                
+
                 case MonoBehaviour mono:
-                    mono.gameObject.SetActive(false);
+                    if(mono.gameObject.activeSelf)
+                        mono.gameObject.SetActive(false);
                     break;
             }
         }
@@ -55,7 +67,7 @@ namespace Maniac.SpawnerSystem
         private void DestroyFunction(T obj)
         {
             if (obj == null) return;
-            
+
             Object.Destroy(obj);
         }
 
